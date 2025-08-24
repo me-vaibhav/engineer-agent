238e986fa25c461645ac89c98ae3a7566b2c00e8
me-vaibhav <164721792+me-vaibhav@users.noreply.github.com>
2025-08-24 22:27:13 +0530

    setting up inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
+import { useTRPC } from "@/trpc/client"
+import { useMutation } from "@tanstack/react-query"
+import { toast } from "sonner"
+
 
 const Page = () => {
+
+const trpc = useTRPC()
+
+const invoke = useMutation(trpc.invoke.mutationOptions({
+  onSuccess:()=>{
+    toast.success("Background job started")
+  }
+}))
+
   return (
-    <Button/>
+  <div className="">
+    <Button disabled={invoke.isPending} onClick={()=>invoke.mutate({text:"testing"})} >
+    Invoke Background Job
+    </Button>
+  </div>
   )
 }
+
+export default Page
